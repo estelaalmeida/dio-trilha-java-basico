4b312d8f48307fc95e616ee4cdaf21a41f64e844
estelaalmeida <estela.almeida.sjc@gmail.com>
2023-09-03 19:42:29 -0300

    Atualizando desafio

---
+++ contaBanco/src/ContaTerminal.java
@@ -0,0 +1,10 @@
+public class ContaTerminal {
+
+
+
+    public static void main(String[] args){
+        System.out.println("Olá [Estela], obrigado por criar uma conta em nosso banco, sua agência é [539], conta [5399] e seu saldo [1.0000.00] já está disponível para saque.");
+    
+    };
+
+}
